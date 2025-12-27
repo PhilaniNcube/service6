67cbfb00ea47269743a93606af97fd165d14e1a6
PhilaniNcube <ncbphi001@gmail.com>
2025-12-27 19:59:00 +0200

    feat: enable filesystem caching for development and build in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   experimental: {
+    // Enable filesystem caching for `next dev`
+    turbopackFileSystemCacheForDev: true,
+    // Enable filesystem caching for `next build`
+    turbopackFileSystemCacheForBuild: true,
+  },
   /* config options here */
   cacheComponents: true,
   typedRoutes: true,
